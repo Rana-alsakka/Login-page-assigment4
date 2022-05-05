c57731fffa1352b47b25e450b6f775e29a6b5858
rana ayman <rana486999@gmail.com>
2022-05-05 15:04:09 +0200

    added change password

--- assigment4/main.cpp
+++ assigment4/main.cpp
@@ -5,14 +5,20 @@ using namespace std;
 void login();
 void registration();
 void forgot ();
+void change_password();
+void check();
+const char *r_password;
+const char *r_password2;
 int main() {
+
     int choice;
     cout << " \t\t\t ------------------------login page------------------------\n";
     cout
             << "1.login" << endl
             << "2.register" << endl
             << "3.forgot password" << endl
-            << "4.exit"<< endl;
+            << "4.change password"<< endl
+            << "5.exit"<<endl;
     cin >> choice;
     switch (choice) {
         case 1:
@@ -25,6 +31,9 @@ int main() {
             forgot();
             break;
         case 4:
+            cout << " ";
+            break;
+        case 5:
             cout << " thank you ";
             break;
         default:
@@ -35,7 +44,7 @@ int main() {
 }
 void login() {
 
-    int count;
+    int count = 0;
     string userId, password, id, pass;
     cout << "please enter the username and password : " << endl;
     cout << "    USERNAME      ";
@@ -50,7 +59,7 @@ void login() {
     }
     input.close();
     if (count == 1) {
-        cout << "      your login is succesfsull!      ";
+        cout << "   welcome "<< userId<<  " your login is successfsull!      ";
         main();
     } else {
         cout << " \n login error \n inccorect username or password";
@@ -61,28 +70,18 @@ void registration()
 {
     string r_userid, r_id , r_pass;
     const char *r_password;
+    const char *r_password2;
     cout<< "      Enter the username :";
     cin>>r_userid;
     cout <<"      Enter the password :";
     cin >> (unsigned char *) r_password;
-    // --------------------------------------------------check for weak password-------
-    if(strlen(r_password) < 8) {
-        cout<< " \n weak password\n    password must be 8 characters long     "<< endl;
-        registration();
-    }
-    else if (strlen(r_password) >= 8) {
-        bool digit_yes = false;
-        bool valid;
-        int len = strlen(r_password);
-        for (int count = 0; count < len; count++)
-            if (isdigit(r_password[count]))
-                digit_yes = true;
-        if (!digit_yes) {
-            valid = false;
-            cout << " \n password must have at least one digit (0-9)" << endl;
-            registration();
-        }
-    }
+    cout << " please re-enter your password";
+    cin >> (unsigned char *) r_password2;
+    if (r_password2 == r_password) {
+        check();}
+         //  else {
+         //  cout << " the passwords you entered doesn't match";
+         //  registration();}
 
     else {
         ofstream f1("records.txt", ios::app);
@@ -96,4 +95,26 @@ void forgot()
     cout<<"\t please choose how you want to recover your password"<< endl;
     cout<< "1.phone number"<<endl<< "2. email"<<endl;
     cin>> option;
+}
+void check() {
+// --------------------------------------------------check for weak password-------
+    if (strlen(r_password) < 8) {
+        cout << " \n weak password\n    password must be 8 characters long     " << endl;
+        registration();
+    } else if (strlen(r_password) >= 8) {
+        bool digit_yes = false;
+        bool valid;
+        int len = strlen(r_password);
+        for (int count = 0; count < len; count++)
+            if (isdigit(r_password[count]))
+                digit_yes = true;
+        if (!digit_yes) {
+            valid = false;
+            cout << " \n password must have at least one digit (0-9)" << endl;
+            registration();
+        }
+    }
+}
+void change_password(){
+
 }
