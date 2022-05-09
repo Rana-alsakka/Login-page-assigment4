ab8c526a719bfc99b9cb4a03e0efbb37a95f9d03
SAHAB MOHAMED ABDELRAHMAN MOHAMED <101561573+Sahab-Mohamed@users.noreply.github.com>
2022-05-09 15:14:11 +0200

    Update login page.h

--- assigment4/login page.h
+++ assigment4/login page.h
@@ -5,13 +5,21 @@
 #include<regex>
 using namespace std;
 
-class loginManager{
+class loginManger{
 private:
-  string usernameAttempt;
+        string usernameAttempt;
         string passwordAttempt;
         bool accessGranted;
   
   
 public:
-  void login()
+  void login();
+  void addUser(string username, string password);
+  int getlastID();
+  void regist();
+  int checkFile(string attempt, const char *p_fileName);
+  void saveFile(string p_line, const char *p_fileName, int i1);
+  long long encrypt(int p_letter);
+  int decrypt(long long p_letter);
+  loginManger();
 }
