5a3a761313616122f3cc2fbc457960cd1f055bf9
SAHAB MOHAMED ABDELRAHMAN MOHAMED <101561573+Sahab-Mohamed@users.noreply.github.com>
2022-05-12 17:38:34 +0200

    Update main text editor.cpp

--- assigment4/main text editor.cpp
+++ assigment4/main text editor.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <cctype>
 #include <string>
+#include <cstring>
 #include <fstream>
 #include "textEditor.h"
+#include "main text editor.h"
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main()
 {
 	char fileName[100];
 	int choice;
+	bool con = true;
 	fstream yourFile;
 	cout << "----------- WILLCOME TO R-M-S Text Editor :) -----------\n\n\n\n";
 	cout << "Please enter the name of the file: ";
@@ -20,42 +22,135 @@ int main()
 	yourFile.open(fileName);
 	if (yourFile.fail()) {
 		cout << "This is a new file. I created it for you :)\n\n\n";
-		textEditor f1;
-		f1.displayMenu();
-		cout << "Enter your choice: ";
-		cin >> choice;
+		fstream yourFile;
+		yourFile.open(fileName , ios::app | ios::in);
 		
-
 		
-
-	}  
+		
+		
+	}
 	else {
 		cout << "The file is already exist.\n\n\n";
-		textEditor f1;
-		f1.displayMenu();
-		cout << "Enter your choice: ";
-		cin >> choice;
-
 		yourFile.close();
-
-	}
-	if (choice == 1) {
-		fstream yourFile;
-		yourFile.open(fileName, ios::app);
-		if (yourFile.is_open()) {
-
-			string newtext;
-			cout << "Enter the new text:";
-			while (true) {
-
-				if (getline(cin, newtext )) {
-					yourFile << newtext << endl;
-				}
-				
-			}
-			yourFile.close();
-		}
+		
 	}
 	
-}
+	
 
+		
+		string newtext;
+
+		while (con == true) {
+			textEditor f1;
+			f1.displayMenu();
+			cout << "Enter your choice: ";
+			cin >> choice;
+			
+
+			switch (choice) {
+			case 1: {
+
+				yourFile.open(fileName, ios::app);
+				if (con == true) {
+
+					system("cls");
+					cout << "Enter the new text:";
+
+					while (newtext == "") {
+						getline(cin, newtext);
+						yourFile << newtext << "\n";
+
+					}
+					yourFile.close();
+					system("cls");
+					cout << "file has been updated succeessfully!\n\n";
+
+					break;
+
+
+				}
+			}
+			case 2: {
+
+				fstream yourFile;
+				yourFile.open(fileName, ios::in);
+				string textt;
+				system("cls");
+				getline(yourFile, textt);
+				while (yourFile) {
+					cout << textt << "\n";
+					getline(yourFile, textt);
+					cout << "\n\n\n\n";
+
+				}
+				yourFile.close();
+				break;
+			}
+
+			case 3: {
+				ofstream yourFile(fileName);
+				system("cls");
+
+				cout << "The file is empty now!\n\n";
+				break;
+			}
+
+			case 4: {
+
+				char c;
+				string temp1;
+				string temp = fileName;
+				ifstream fin;
+				ofstream fout;
+				fin.open(temp.c_str(), ios::binary);
+				temp = temp + " Encrypted file";
+				fout.open(temp.c_str(), ios::binary);
+
+				while (!fin.eof()) {
+				
+					fin >> noskipws >> c;
+					int tempo = (c + 1);
+					fout << (char)tempo;
+				}
+				fin.close();
+				fout.close();
+
+				system("cls");
+				cout << "New encrypted file has been updated succeessfully!\n\n";
+
+				break;
+			}
+			case 5: {
+
+				char c;
+				string temp = fileName;
+				ifstream fin;
+				ofstream fout;
+				fin.open(temp.c_str(), ios::binary);
+				temp = temp + " Decrypted file";
+				fout.open(temp.c_str(), ios::binary);
+				
+
+				while (!fin.eof()) {
+
+					fin >> noskipws >> c;
+					int tempo = (c - 1);
+					fout << (char)tempo;
+				}
+				fin.close();
+				fout.close();
+
+				system("cls");
+				cout << "New decrypted file has been updated succeessfully!\n\n";
+
+				break;
+			}
+
+
+
+				} //switch bracket
+
+			}//while bracket
+
+		
+	}// main bracket
