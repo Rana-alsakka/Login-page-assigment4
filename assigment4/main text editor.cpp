a14ed22a197ede5c8619b36aae4215282bf16a5b
Sahab Mohamed <sahab.boba20003@gmail.com>
2022-05-13 17:34:23 +0200

    till req7

--- assigment4/main text editor.cpp
+++ assigment4/main text editor.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstring>
 #include <fstream>
+#include <stdio.h>
 #include "textEditor.h"
 #include "main text editor.h"
 
@@ -12,7 +13,7 @@ using namespace std;
 int main()
 {
 	char fileName[100];
-	int choice;
+	int choice = 0;
 	bool con = true;
 	fstream yourFile;
 	cout << "----------- WILLCOME TO R-M-S Text Editor :) -----------\n\n\n\n";
@@ -23,52 +24,55 @@ int main()
 	if (yourFile.fail()) {
 		cout << "This is a new file. I created it for you :)\n\n\n";
 		fstream yourFile;
-		yourFile.open(fileName , ios::app | ios::in);
-		
-		
-		
-		
+		yourFile.open(fileName, ios::app | ios::in);
+
+
+
+
 	}
 	else {
 		cout << "The file is already exist.\n\n\n";
 		yourFile.close();
-		
+
 	}
-	
-	
 
-		
-		string newtext;
 
-		while (con == true) {
-			textEditor f1;
-			f1.displayMenu();
-			cout << "Enter your choice: ";
-			cin >> choice;
-			
+
+
+	string newtext;
+
+	while (con == true) {
+		textEditor f1;
+		f1.displayMenu();
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+
+		if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6 || choice == 7 || choice == 8 || choice == 9 || choice == 10 || choice == 11 || choice == 12 || choice == 13 || choice == 14 || choice == 15 || choice == 16) {
+
 
 			switch (choice) {
 			case 1: {
 
 				yourFile.open(fileName, ios::app);
-				if (con == true) {
 
-					system("cls");
-					cout << "Enter the new text:";
 
-					while (newtext == "") {
-						getline(cin, newtext);
-						yourFile << newtext << "\n";
+				system("cls");
+				cout << "Enter the new text:";
 
-					}
-					yourFile.close();
-					system("cls");
-					cout << "file has been updated succeessfully!\n\n";
-
-					break;
-
+				while (newtext == "") {
+					getline(cin, newtext);
+					yourFile << newtext << "\n";
 
 				}
+				yourFile.close();
+				system("cls");
+				cout << "file has been updated succeessfully!\n\n";
+
+				break;
+
+
+
 			}
 			case 2: {
 
@@ -107,7 +111,7 @@ int main()
 				fout.open(temp.c_str(), ios::binary);
 
 				while (!fin.eof()) {
-				
+
 					fin >> noskipws >> c;
 					int tempo = (c + 1);
 					fout << (char)tempo;
@@ -129,7 +133,7 @@ int main()
 				fin.open(temp.c_str(), ios::binary);
 				temp = temp + " Decrypted file";
 				fout.open(temp.c_str(), ios::binary);
-				
+
 
 				while (!fin.eof()) {
 
@@ -145,12 +149,64 @@ int main()
 
 				break;
 			}
+			case 6: {
+
+				char file2[100], ch;
+
+				fstream yourFile, fileTwo;
+				cout << "Enter the name of the file that you want to merge with: ";
+				cin >> file2;
+				yourFile.open(fileName, ios::app);
+				fileTwo.open(file2, ios::in);
+
+				if (!fileTwo) {
+
+					cout << "An error has occurred while opening file! ";
+				}
+				else {
+					while (fileTwo >> noskipws >> ch) {
+						yourFile << ch;
+					}
+					yourFile.close();
+					fileTwo.close();
+
+					system("cls");
+					cout << "Files have merged successfully.\n\n\n";
+					break;
+				}
+			}
+			case 7: {
+				fstream yourFile;
+				yourFile.open(fileName, ios::in);
+				char ch;
+				int word = 0;
+				yourFile.seekg(0, ios::beg);
+
+				while (yourFile) {
+					yourFile.get(ch);
+					if (ch == ' ' || ch == '\n')
+						word++;
+				}
+				yourFile.close();
+				system("cls");
+				cout << "File words = " << word << '\n\n\n\n';
+
+
+			}
+
+			}//switch bracket
 
 
 
-				} //switch bracket
 
-			}//while bracket
+		}// if statement for checking choice  
+		else {
 
-		
-	}// main bracket
+			cout << "Wrong input, please choose between the menu !";
+		}
+
+	}//while bracket
+
+
+}// main bracket
+
