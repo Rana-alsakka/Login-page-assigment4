8839c2731e3023493c9a69198d8ecf7af8e93d2a
Rana486 <100386390+Rana486@users.noreply.github.com>
2022-05-16 23:27:14 +0200

    Update login page.cpp

--- assigment4/login page.cpp
+++ assigment4/login page.cpp
@@ -4,48 +4,49 @@
 #include<sstream>
 #include<regex>
 #include"login page.h"
+#include<conio.h>
 void login() {
-     int x = 1;
-     while (x < 3) {  // login attempts
-         cout << "please enter your username and password" << endl << " username" << endl;
-         cin >> userNameAttempt;
+        int x = 1;
+        while (x < 3) {  // login attempts
+            cout << "please enter your username and password" << endl << " username" << endl;
+            cin >> userNameAttempt;
 
-         int usrID = checkFile(userNameAttempt, "users.dat"); //check for username
-         if (usrID != 0) { //if found
-             cout << "Password:";
-		 
-		  char passwordAttempt[16] = { 0 };
- 	  	 int i;
-   		 for (i = 0; i < 16;i++) {
-     	  	 passwordAttempt[i] = _getch(); _putch('*');
-       		
-		 
-		 
-             cin >> passwordAttempt;
-             int pwdID = checkFile(passwordAttempt, "pswds.dat");
-             if (usrID == pwdID) { // check if the password belongs to this username
-                 cout << "hello " << userNameAttempt << " you are successfully logged in!" << endl;
-                 x = 0;
-                 break;
-             } else {
-                 cout << "wrong username please try again" << endl;
-                 x++;
+            int usrID = checkFile(userNameAttempt, "users.dat"); //check for username
+            if (usrID != 0) { //if found
+                cout << "Password:";
 
-                 if (x > 3) {
-                     cout
-                             << "you've reached the maximum limit for attempting password , you are denied to access the system"
-                             << endl;
-                     break;
-                 } else {
-                     x++;
-                     cout << x;
-                 }
-             }
-         } else {
-             cout << "wrong password please try again" << endl;
-         }
-     }
- }
+                char passwordAttempt[16] = { 0 };
+                int i;
+                for (i = 0; i < 16;i++) {
+                    passwordAttempt[i] = _getch(); _putch('*');
+
+                }
+
+                cin >> passwordAttempt;
+                int pwdID = checkFile(passwordAttempt, "pswds.dat");
+                if (usrID == pwdID) { // check if the password belongs to this username
+                    cout << "hello " << userNameAttempt << " you are successfully logged in!" << endl;
+                    x = 0;
+                    break;
+                } else {
+                    cout << "wrong username please try again" << endl;
+                    x++;
+
+                    if (x > 3) {
+                        cout
+                                << "you've reached the maximum limit for attempting password , you are denied to access the system"
+                                << endl;
+                        break;
+                    } else {
+                        x++;
+                        cout << x;
+                    }
+                }
+            } else {
+                cout << "wrong password please try again" << endl;
+            }
+        }
+    }
     //-----------------------------for adding users and saving them-----------------------------------
     void saveFile()
     {
@@ -95,9 +96,23 @@ void login() {
         file.close();
         cout << "please enter a password" << endl;
         cout << " *passwords must be 8 characters long & have at least one uppercase character and a number "<<endl;
+
+        int i;
+        for (i = 0; i < 16;i++) {
+            password[i] = _getch(); _putch('*');
+
+        }
         cin >> password;
         cout << "please renter a password" << endl;
         cin >> pass2;
+        int x;
+        for (x = 0; i < 16;x++) {
+            pass2 = _getch(); _putch('*');
+
+        }
+
+
+
         if (password == pass2) { // if password confirmation matches
             checkPass(password);  // check for strong password
             int id =   getLastID();
@@ -127,7 +142,7 @@ void login() {
 
     }
 
- //------------------------saving users and passwords information with id-----------------------------------
+    //------------------------saving users and passwords information with id-----------------------------------
     int getLastID()
     {
         fstream file;
@@ -198,49 +213,69 @@ void login() {
         }
     }
 //-----------------------------change password-----------------------------------
-void change() {
-    string newpass;
-    string newpass2;
-    cout << " please enter your old password";
-    cin >> passwordAttempt;
-    int usrID = checkFile(userNameAttempt, "users.dat");
-    int pwdID = checkFile(passwordAttempt, "pswds.dat");
-    if (usrID == pwdID) {
-        cout << "please enter your new password";
-        cin >> newpass;
-        cout << "please renter a password" << endl;
-        cin >> newpass2;
-        if (newpass2 == newpass) { // check if passwords match
-            checkPass(newpass); // check for strong password
-           if (newpass == passwordAttempt ){
-               cout << "you can't use an old password"<< endl; //check for old password
-               change();
-               return;
-           }
-        int id = pwdID;     // to save to the same id for the user not new one
-        fstream file;
-        file.open("pswds.dat", ios::app);
-        file.seekg(0, ios::end);
+    void change() {
+        string newpass;
+        string newpass2;
+        cout << " please enter your old password";
+        cin >> passwordAttempt;
+        int usrID = checkFile(userNameAttempt, "users.dat");
+        int pwdID = checkFile(passwordAttempt, "pswds.dat");
+        if (usrID == pwdID) {
+            cout << "please enter your new password";
 
-        if(file.tellg() != 0)
-            file << "\n";
 
-        file.seekg(0, ios::beg);
+            int i;
+            for (i = 0; i < 16;i++) {
+                newpass[i] = _getch(); _putch('*');
 
-        for(int i = 0; i < newpass.length(); i++)
-        {
-            file << encrypt(newpass[i]); // loop for encrypting password
-            file << "\n";
-        }
+            }
 
-        file << "#ID:" << id;
-        file.close();
-    cout<< " your password is successfully changed";
-    }
-        else {
-            cout<< "passwords don't match please try again";
-        }
-}}
+            cin >> newpass;
+            cout << "please renter a password" << endl;
+
+            newpass2[16] = { 0 };
+            int u;
+            for (u = 0; i < 16;i++) {
+                newpass2[u] = _getch(); _putch('*');
+
+            }
+
+
+
+            cin >> newpass2;
+            if (newpass2 == newpass) { // check if passwords match
+                checkPass(newpass); // check for strong password
+                if (newpass == passwordAttempt ){
+                    cout << "you can't use an old password"<< endl; //check for old password
+                    change();
+                    return;
+                }
+                int id = pwdID;     // to save to the same id for the user not new one
+                fstream file;
+                file.open("pswds.dat", ios::app);
+                file.seekg(0, ios::end);
+
+                if(file.tellg() != 0)
+                    file << "\n";
+
+                file.seekg(0, ios::beg);
+
+
+
+                for(int i = 0; i < newpass.length(); i++)
+                {
+                    file << encrypt(newpass[i]); // loop for encrypting password
+                    file << "\n";
+                }
+
+                file << "#ID:" << id;
+                file.close();
+                cout<< " your password is successfully changed";
+            }
+            else {
+                cout<< "passwords don't match please try again";
+            }
+        }}
 //-----------------------------encryption and decryption-----------------------------------
     long long encrypt(int p_letter)
     {
@@ -293,47 +328,47 @@ void change() {
 
         return 0;
     }
-  //----------checking for  Email----------------------
-  int validEmail(string email) {
-      if (regex_match(email, regex("([a-z]+)([_.a-z0-9]*)([a-z0-9]+)(@)([a-z]+)([.a-z]+)([a-z]+)"))){
-          return 0;
-      }
-      else {
-          cout << "wrong email format\n";
-          saveFile();
-      }
-  }
-  // -------------checking for phone number-----
-  int phoneValidation(string phone)
-{
-	char start = '0';
-	char start2= '1';
-
-	if (phone.length() != 11)			//check for length
-	{
-        cout<< "wrong phonenumber format , missing numbers";
-        showmenue();
-	}
-
-    for( int count = 0; count < phone.length(); count++ )	//loop to check each individual character
+    //----------checking for  Email----------------------
+    int validEmail(string email) {
+        if (regex_match(email, regex("([a-z]+)([_.a-z0-9]*)([a-z0-9]+)(@)([a-z]+)([.a-z]+)([a-z]+)"))){
+            return 0;
+        }
+        else {
+            cout << "wrong email format\n";
+            saveFile();
+        }
+    }
+    // -------------checking for phone number-----
+    int phoneValidation(string phone)
     {
-    	if (!isdigit(phone[count]))		//checks all characters in phone if they are not digits
-    	{
-            cout<< "wrong phonenumber format , must be digits";
+        char start = '0';
+        char start2= '1';
+
+        if (phone.length() != 11)			//check for length
+        {
+            cout<< "wrong phonenumber format , missing numbers";
             showmenue();
-		}
-		if (count==0 && phone[count] != start || count==1 && phone[count] != start2)//if it doesn't start with 01
-		{
-            cout<< "wrong phonenumber format , must start with (01)";
-            showmenue();
-		}
-		else{
-		return 0;
-	}}
-    return 0;
-}
+        }
+
+        for( int count = 0; count < phone.length(); count++ )	//loop to check each individual character
+        {
+            if (!isdigit(phone[count]))		//checks all characters in phone if they are not digits
+            {
+                cout<< "wrong phonenumber format , must be digits";
+                showmenue();
+            }
+            if (count==0 && phone[count] != start || count==1 && phone[count] != start2)//if it doesn't start with 01
+            {
+                cout<< "wrong phonenumber format , must start with (01)";
+                showmenue();
+            }
+            else{
+                return 0;
+            }}
+        return 0;
+    }
 //-----------check username format--------
- int userFormat( string username){
+    int userFormat( string username){
         bool number_case = false;
         bool special_char = false;
 
@@ -364,39 +399,38 @@ void change() {
         } while (!done);
 
         return 0;
+    }
+
+    void showmenue(){
+        int choice;
+        cout << " \t\t\t ------------------------login page------------------------\n";
+        cout
+                << "1.login" << endl
+                << "2.register" << endl
+                << "3.change password" << endl
+                << "4.exit"<< endl;
+        cin >> choice;
+        if( choice == 1) {
+            LoginManager app;
+            app.login();
+            cin.get();
         }
+        else if (choice ==2) {
+            //  registration();
+            LoginManager loginMangerObj;
+            loginMangerObj.saveFile();
 
-  void showmenue(){
-      int choice;
-      cout << " \t\t\t ------------------------login page------------------------\n";
-      cout
-              << "1.login" << endl
-              << "2.register" << endl
-              << "3.change password" << endl
-              << "4.exit"<< endl;
-      cin >> choice;
-      if( choice == 1) {
-          LoginManager app;
-          app.login();
-          cin.get();
-      }
-      else if (choice ==2) {
-          //  registration();
-          LoginManager loginMangerObj;
-          loginMangerObj.saveFile();
+        }
+        else if (choice ==3) {
+            //  change password();
+            LoginManager app;
+            app.login();
+            app.change();
 
-      }
-      else if (choice ==3) {
-          //  change password();
-          LoginManager app;
-          app.login();
-          app.change();
-
-      }
-      else if (choice ==4) {
-          cout << " thank you ";
-      }
-      else
-          cout << " \t\t\t please select from the options above";
-  }
-
+        }
+        else if (choice ==4) {
+            cout << " thank you ";
+        }
+        else
+            cout << " \t\t\t please select from the options above";
+    }
